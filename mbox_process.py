0e68bb3b46c3166ef79184592b994590b6f2403c
Wayne Piekarski <wayne@tinmith.net>
2022-07-01 00:42:42 -0700

    Use common writing function that handles duplicate filenames, decode exception bug was in other places that need to use shared charset decoding

--- mbox_process.py
+++ mbox_process.py
@@ -4,6 +4,49 @@ import csv
 import argparse
 import sys
 
+
+def write_output(folder_name, in_file_name, write_data, symlink=False):
+    # Remove bad characters from the attachment file name
+    file_name = ''.join([s for s in in_file_name if s in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,-_ ~`!@#$%^&+=<>'])
+
+    dupstr = ""
+    dup = 0
+    while True:
+        filepath = os.path.join(folder_name, dupstr + file_name)
+        if os.path.exists(filepath):
+            dup = dup + 1
+            dupstr = str(dup) + "-"
+        else:
+            break
+        if dup == 10:
+            sys.exit(f"FATAL: Reached 10 duplicates for [{folder_name}] attachment [{attachment_name}]")
+    if symlink:
+        os.symlink("../" + filepath, folder_name + "-" + dupstr + file_name)
+    # Need to convert any strings to UTF-8 so they can be written in "wb" mode
+    if isinstance(write_data, str):
+        write_data = write_data.encode('utf-8')
+    open(filepath, "wb").write(write_data)
+
+
+def safe_decode(payload, charset):
+    try:
+        body = payload.decode(encoding=charset)
+    except Exception as e:
+        sys.exit(f"Exception decoding {idx:04}: [{e}]")
+    return body
+
+
+def safe_charset(part):
+    content_charset = part.get_content_charset()
+    if content_charset is None:
+        # Python needs a charset, so pick a suitable default if none provided
+        content_charset = "iso-8859-1"
+    elif content_charset == "us-ascii" or content_charset == "ascii":
+        # Some emails in us-ascii actually contain non-ascii data, so pick a more useful charset to handle this
+        content_charset = "iso-8859-1"
+    return content_charset
+
+
 ############################
 ### PARSE CLI ARGUEMENTS ###
 ############################
@@ -98,13 +141,7 @@ SUBJECT: {message['subject']}</br>
             # get email content
             content_type = part.get_content_type()
             content_disposition = str(part.get("Content-Disposition")).replace('\n',' ')
-            content_charset = part.get_content_charset()
-            if content_charset is None:
-                # Python needs a charset, so pick a suitable default if none provided
-                content_charset = "iso-8859-1"
-            elif content_charset == "us-ascii" or content_charset == "ascii":
-                # Some emails in us-ascii actually contain non-ascii data, so pick a more useful charset to handle this
-                content_charset = "iso-8859-1"
+            content_charset = safe_charset(part)
             print(f"Msg {idx:04}: get_content_type={content_type}, Content-Disposition={content_disposition}, get_content_charset={part.get_content_charset()}-->{content_charset}")
             payload = part.get_payload(decode=True)
             # https://docs.python.org/3/library/email.compat32-message.html#email.message.Message.get_payload
@@ -113,90 +150,68 @@ SUBJECT: {message['subject']}</br>
                 # Skip this part because payload.decode() will fail otherwise
                 # print(f"Skipping None multipart payload")
                 continue
-            try:
-                body = payload.decode(encoding=content_charset)
-            except Exception as e:
-                print(f"Exception in {idx:04}: {body}")
-                # Fail on exceptions, but could alternative skip and go to the next part
-                sys.exit(1)
-                # continue
 
             # save plain text of email
             if content_type == "text/plain":
                 # add the body of the message
+                body = safe_decode(payload, content_charset)
                 full_message_text = full_message + f"{body}"
 
                 # write the message to a file
-                # name the file
                 filename = f"msg-{idx:04}.txt"
-                filepath = os.path.join(folder_name, filename)
-                # check if file exists to append
-                if os.path.exists(filepath):
-                    # append additional text to existing file
-                    open(filepath, "a").write(body)
-                else:
-                    # write the file
-                    open(filepath, "w").write(full_message_text)
+                # Previously was writing {body} if the file existed already
+                write_output(folder_name, filename, full_message_text)
 
             # save html text of email
             elif content_type == "text/html":
                 # add the body of the message
+                body = safe_decode(payload, content_charset)
                 full_message_html = html_header + f"CONTENT: \n{body}"
 
                 # write the message to a file
-                # name the file
                 filename = f"msg-{idx:04}.html"
-                filepath = os.path.join(folder_name, filename)
-                # write the file
-                open(filepath, "a+").write(full_message_html)
+                write_output(folder_name, filename, full_message_html)
 
             # multipart/mixed messages
             elif content_type == "multipart/mixed":
                 # add the body of the message
+                body = safe_decode(payload, content_charset)
                 full_message_mixed = full_message + f"CONTENT: \n{body}"
 
                 # write the message to a file
-                # name the file
                 filename = f"msg-{idx:04}-mxd.html"
-                filepath = os.path.join(folder_name, filename)
-                # write the file
-                open(filepath, "w").write(full_message_mixed)
+                write_output(folder_name, filename, full_message_mixed)
 
             # save png attachments
             elif content_type == "image/png":
                 # update email summary list
                 msg_dict_temp['PNG'] = "Y"
-                # download attachment
                 attachment_name = part.get_filename()
                 if attachment_name:
-                    filepath = os.path.join(folder_name, attachment_name)
-                    # download attachment and save it
-                    open(filepath, "wb").write(part.get_payload(decode=True))
-                    os.symlink("../" + filepath, folder_name + "-" + attachment_name)
+                    write_output(folder_name, attachment_name, part.get_payload(decode=True), symlink=True)
 
             # save jpg attachments
             elif content_type == "image/jpeg":
                 # update email summary list
                 msg_dict_temp['JPG'] = "Y"
-                # download attachment
                 attachment_name = part.get_filename()
                 if attachment_name:
-                    filepath = os.path.join(folder_name, attachment_name)
-                    # download attachment and save it
-                    open(filepath, "wb").write(part.get_payload(decode=True))
-                    os.symlink("../" + filepath, folder_name + "-" + attachment_name)
+                    write_output(folder_name, attachment_name, part.get_payload(decode=True), symlink=True)
 
             # save email attachment
             elif "attachment" in content_disposition:
                 # update email summary list
                 msg_dict_temp['Attachment'] = "Y"
-                # download attachment
                 attachment_name = part.get_filename()
                 if attachment_name:
-                    filepath = os.path.join(folder_name, attachment_name)
-                    # download attachment and save it
-                    open(filepath, "wb").write(part.get_payload(decode=True))
-                    os.symlink("../" + filepath, folder_name + "-" + attachment_name)
+                    write_output(folder_name, attachment_name, part.get_payload(decode=True), symlink=True)
+
+            # save any other attachments that have a filename
+            elif part.get_filename() != None and part.get_filename() != "":
+                write_output(folder_name, part.get_filename(), part.get_payload(decode=True), symlink=True)
+
+            else:
+                print(f"Ignoring item which had no match in if statements, attachment has no filename")
 
         # append final message dict to email summary list
         email_list.append(msg_dict_temp)
@@ -204,38 +219,24 @@ SUBJECT: {message['subject']}</br>
     else:
         if message.get_content_type() == "text/plain":
             for part in message.walk():
-                # add the body of the message
-                try:
-                    # get the email body
-                    body = part.get_payload(decode=True).decode()
-                except:
-                    pass
+                payload = part.get_payload(decode=True)
+                body = safe_decode(payload, safe_charset(part))
+
                 full_message_text = full_message + f"{body}"
 
-                # write the message to a file
-                # name the file
                 filename = f"msg-{idx:04}.txt"
-                filepath = os.path.join(folder_name, filename)
-                # write the file
-                open(filepath, "a+").write(full_message_text)
+                write_output(folder_name, filename, full_message_text)
         elif message.get_content_type() == "text/html":
             for part in message.walk():
-                # add the body of the message
-                try:
-                    # get the email body
-                    body = part.get_payload(decode=True).decode()
-                except:
-                    pass
+                payload = part.get_payload(decode=True)
+                body = safe_decode(payload, safe_charset(part))
+
                 full_message_text = html_header + f"CONTENT: \n{body}"
 
-                # write the message to a file
-                # name the file
                 filename = f"msg-{idx:04}.html"
-                filepath = os.path.join(folder_name, filename)
-                # write the file
-                open(filepath, "a+").write(full_message_text)
+                write_output(folder_name, filename, full_message_text)
         else:
-            print(f"MESSAGE SKIPPED {idx:04}")
+            sys.exit(f"MESSAGE SKIPPED {idx:04} {message.get_content_type()}")
 
 # write email summary to csv
 with open(f'{dir_name}/{dir_name}.csv', 'w') as csvfile:
